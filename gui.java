7c0eb656d140ef4aa2e3f06e4ef447797f5a0859
ZJawadi <zjawadi1@gmail.com>
2021-08-21 16:49:58 -0400

    second gui

--- gui.java
+++ gui.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 
 class gui {
-    public void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         new gui(); 
     }
 
@@ -14,6 +14,10 @@ class gui {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(700,700);
         frame.setVisible(true);
+        JLabel welcome = new JLabel("Hello! Welcome to Typing Test. You will be given 10 words to type.\n" +
+        "Please type in the word after you hear it and hit \"Enter\". You will hear a sound indicating if you typed it correctly.\n" +
+        "You will get your final score at the end of the game. Good luck and happy typing!");
+        frame.add(welcome);
     }
 
 }
