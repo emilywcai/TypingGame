ba6d39539a07061813469c4d9e3952709990f38a
Emily Cai <emilyw.cai@gmail.com>
2021-08-21 16:18:20 -0400

    logic

--- TypingQuiz.java
+++ TypingQuiz.java
@@ -1,7 +1,40 @@
+import java.util.Scanner;
 public class TypingQuiz
 {
+    private String[] words = 
+    {"blue", "hackathon", "water", "purple", "keyboard", "telephone", "dolphin", "popsicle", "cardboard", "confetti"};
+    private int score = 0;
+
+    public boolean evaluate(String attempt, int num)
+    {
+        if (attempt.equalsIgnoreCase(words[num]))
+             return true;
+        else
+            return false;
+    }
+
+    public void runGame()
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hello! Welcome to Typing Test. You will be given 10 words to type.\n" +
+                            "Please type in the word after you hear it and hit \"Enter\". You will hear a sound indicating if you typed it correctly.\n" +
+                            "You will get your final score at the end of the game. Good luck and happy typing!");
+
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println("Please type in " + words[i]);
+            String attempt = input.nextLine();
+
+            if (evaluate(attempt, i))
+                score++;
+        }
+
+        System.out.println("Your score is " + score + "/10");
+    }
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        TypingQuiz game = new TypingQuiz();
+        game.runGame();
     }
 }
