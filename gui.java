718bd4f7a9471e7b2d2b57f959245d0265e42f8c
ZJawadi <zjawadi1@gmail.com>
2021-08-21 16:38:46 -0400

    first gui

--- gui.java
+++ gui.java
@@ -1,5 +1,19 @@
+import javax.swing.*;
+
 class gui {
-    public static void main(String[] args) throws Exception {
-        System.out.println("bruh");
+    public void main(String[] args) throws Exception {
+        new gui(); 
     }
+
+    gui(){
+        welcome();
+    }
+
+    public void welcome() {
+        JFrame frame = new JFrame("Welcome!");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(700,700);
+        frame.setVisible(true);
+    }
+
 }
